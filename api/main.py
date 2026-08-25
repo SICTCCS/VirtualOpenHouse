b330228b7e0cae53fc2335b7117a253c2e1e40d9
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2026-08-25 14:55:35 -0500

    Add rest of the APIs and importing

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from apscheduler.schedulers.background import BackgroundScheduler
-import yaml
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
-import dotenv
+import requests
+
+import json
+from ruamel.yaml import YAML
+import os, io, dotenv
 
 from typing import List, Dict
 
@@ -14,12 +16,18 @@ app: Flask = Flask(__name__)
 app.logger.setLevel("DEBUG")
 l = app.logger
 
+yaml = YAML()
+yaml.preserve_quotes = True
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.width = 75
+
 SCOPES = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", SCOPES)
 client = gspread.authorize(creds)
+# fetches data from the Google Sheet and updates the corresponding markdown files
 def get_sheet_data():
     l.info("Fetching sheet data...")
     spread = client.open_by_key(os.getenv("SHEET_ID"))
@@ -29,48 +37,175 @@ def get_sheet_data():
     handle_teacher_data(spread.get_worksheet(2).get_all_records())
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=get_sheet_data, trigger="interval", seconds=60)
+scheduler.add_job(func=get_sheet_data, trigger="interval", seconds=1800)
 scheduler.start()
 
+################### routes
+
+# placeholder route for the home page
 @app.route("/")
 def home():
     return "Hello, nothing to see here :|"
 
-@app.route("/api")
+# receives data from the Google Apps Script and updates the corresponding markdown files
+@app.route("/api", methods=["POST"])
 def api():
     if request.headers.get("Authorization") != os.environ.get("SECRET"):
         return "Unauthorized", 401
-    # TODO: Implement the actual API logic here
-    sheet_number = request.args.get("sheet")
-    # send to respective function
+    sheet = int(request.args.get("sheet"))
+    data = request.get_json()
+    l.info(f"Received data for sheet {sheet}")
+    match sheet:
+        case 0:
+            handle_course_data(data)
+        case 1:
+            handle_class_data(data)
+        case 2:
+            handle_teacher_data(data)
     return "API endpoint accessed successfully"
 
-COURSE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "_courses")
+############### utils
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+COURSE_PATH = os.path.join(ROOT_DIR, "_courses")
+
+def get_file_path_from_slug(slug: str) -> str:
+    return os.path.join(COURSE_PATH, f"{slug}.md")
+
+def load_yaml_frontmatter(slug: str) -> Dict[str, int | float | str]:
+    yml = load_yaml_frontmatter_raw(slug)
+    yml['slug'] = slug
+    return yml
+
+def load_yaml_frontmatter_raw(slug: str) -> Dict[str, int | float | str]:
+    filepath = get_file_path_from_slug(slug)
+    if not os.path.exists(filepath):
+        return {}
+    with open(filepath, 'r', encoding='utf-8') as f:
+        content = f.read().strip()
+    if not content.startswith('---\n') and not content.startswith('---\r\n'):
+        return {}
+    
+    # maxsplit=2 splits it into: ['', 'frontmatter content', 'markdown content']
+    parts = content.split('---', 2)
+    if len(parts) >= 3:
+        return yaml.load(parts[1])
+    return {}
+
+def save_yaml_frontmatter(slug: str, yml):
+    if not yml: 
+        l.error("Save data empty")
+        return
+    filepath = get_file_path_from_slug(slug)
+    with open(filepath, "w", encoding="utf8") as f, io.StringIO() as stream:
+        # stream = io.StringIO()
+        stream.write("---\n")
+        yaml.dump(yml, stream)
+        stream.write("---")
+        txt = stream.getvalue()
+        # stream.close()
+        if txt.strip(): f.write(txt)
+
+def update_all(to_update, data: dict):
+    for key in data.keys():
+        if key == "slug": continue
+        to_update[key] = data[key]
+    return to_update
+
+############### save handlers
 
 def handle_course_data(data: List[Dict[str, int | float | str]]):
-    l.info(f"Handling course data: {data}")
+    # l.info(f"Handling course data: {data}")
     for course in data:
         slug = course.get("slug")
-        l.info(f"Processing course: {slug}")
-        l.info(COURSE_PATH)
-        with open(f"{COURSE_PATH}/{slug}.md", "w") as f:
-            lines = f.readlines()[1:-1] # ignore first and last lines
-            yml = yaml.safe_load("\n".join(lines))
-        l.info(f"Course YAML data: {yml}")
+        l.info("Saving course info for: " + slug)
+        yml = load_yaml_frontmatter(slug)
+        yml = update_all(yml, course)
         
+        img_path = os.path.join(ROOT_DIR, "assets", "image", "courses", slug)
+        if os.path.exists(img_path):
+            files = os.listdir(img_path)
+            img_num = len([f for f in files if f.endswith(".jpg")])
+        else: img_num = 0
+        if not "course" in yml: yml["course"] = {}
+        yml["course"]["image_num"] = img_num
+        
+        save_yaml_frontmatter(slug, yml)
 
 def handle_class_data(data: List[Dict[str, int | float | str]]):
-    l.info(f"Handling class data: {data}")
+    # l.info(f"Handling class data: {data}")
     for class_record in data:
         slug = class_record.get("slug")
-        l.info(f"Processing class: {slug}")
+        l.info("Saving class info for: " + slug)
+        yml = load_yaml_frontmatter(slug)
+        yml = update_all(yml, class_record)
+        save_yaml_frontmatter(slug, yml)
+        
 
 def handle_teacher_data(data: List[Dict[str, int | float | str]]):
-    l.info(f"Handling teacher data: {data}")
+    # l.info(f"Handling teacher data: {data}")
+    course_teachers: dict[str, list[dict]] = {}
+    
     for teacher in data:
         slug = teacher.get("slug")
-        l.info(f"Processing teacher: {slug}")
+        teachers: list[dict] = course_teachers.get(slug, [])
+        teachers.append(update_all({}, teacher))
+        course_teachers[slug] = teachers
+    for course in course_teachers.keys():
+        teachers = course_teachers[course]
+        l.info("Saving teacher for: " + course)
+        yml = load_yaml_frontmatter(course)
+        yml["teachers"] = teachers
+        save_yaml_frontmatter(course, yml)
 
+############### send data to Google Apps Script to update the Google Sheet
 
-get_sheet_data() # Initial fetch
+URL = os.environ.get("GOOGLE_APPS_SCRIPT_URL")
+def send_to_sheet(data, sheet):
+    response = requests.post(f"{URL}?sheet={sheet}", json=json.dumps(data), headers={"Authorization": os.environ.get("SECRET")})
+    l.info(f"Response from Google Apps Script: {response.status_code} - {response.text}")
+
+def import_to_spread(slug):
+    l.info(f"Importing data for slug: {slug}")
+    yml = load_yaml_frontmatter(slug)
+    data = [
+        yml.get("slug"),
+        yml.get("title"),
+        yml.get("description"),
+        yml.get("short_description")
+    ]
+    send_to_sheet(data, 0)
+    data = [
+        yml.get("slug"),
+        yml.get("kuula_id")
+    ]
+    send_to_sheet(data, 1)
+    for teacher in yml.get("teachers", []):
+        data = [
+            yml.get("slug"),
+            teacher.get("name"),
+            teacher.get("program"),
+            teacher.get("degree"),
+            teacher.get("career_start"),
+            teacher.get("certifications"),
+            teacher.get("email"),
+            teacher.get("phone"),
+            teacher.get("room"),
+            teacher.get("love_teaching"),
+            teacher.get("fun_fact"),
+        ]
+        send_to_sheet(data, 2)
+
+def import_all_to_spread():
+    for filename in os.listdir(COURSE_PATH):
+        if filename.endswith(".md"):
+            slug = filename[:-3] # remove .md
+            import_to_spread(slug) 
+
+############# Run stuff
+
+# import_all_to_spread() # Initial import
+# import_to_spread("PMT")
+
+# get_sheet_data() # Initial fetch
 app.run(host="0.0.0.0", port=5000, debug=True)
